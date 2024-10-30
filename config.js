d6a60b66732e125e4ca2fe2195182308005e0f7b
senusnake <senusnake@gmail.com>
2024-10-30 21:36:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || 'bOoETCaC#O5ih_GVL2FGoNBuSt-Fb50pekZdM-95l1kpbRa1LmHE', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb://mongo:tpezcbNDAyrJpydDaRBXeGFOpForJyDP@junction.proxy.rlwy.net:45774',// Enter Your MongoDB URL
+    Owner: process.env.OwnerNumber || '94766950645',    // Enter Your Owner Number
 };
